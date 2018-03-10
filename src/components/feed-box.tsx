7ec71ab150a7599e3becab3b2fe5d5084290565a
Skyler Olson <skyler.olson.rl@gmail.com>
2018-03-09 20:47:56 -0800

    Animation's pretty good

--- src/components/feed-box.tsx
+++ src/components/feed-box.tsx
@@ -5,28 +5,58 @@ export interface FeedBoxProps {
 }
 
 export interface FeedBoxState {
-    expanded: boolean;
+    show: boolean;
+    animating: boolean;
+    mainStyles: React.CSSProperties;
 }
 
 export class FeedBox extends React.Component<FeedBoxProps, FeedBoxState> {
     state: FeedBoxState = {
-        expanded: false
+        show: false,
+        animating: false,
+        mainStyles: { maxHeight: 0 }
     };
+
+    mainDiv: HTMLDivElement;
 
     constructor(props: FeedBoxProps) {
         super(props);
     }
 
+    watchMain = (mainDivParam: HTMLDivElement | null) => {
+        if (mainDivParam instanceof HTMLDivElement) {
+            this.mainDiv = mainDivParam;
+            const childChangeObserver = new MutationObserver(() => {
+                const targetHeight = Math.min(500, this.mainDiv.scrollHeight);
+                this.setState({ mainStyles: { maxHeight: targetHeight } });
+            });
+            childChangeObserver.observe(this.mainDiv, { childList: true, subtree: true });
+        } else {
+            throw 'Main div must not be null';
+        }
+    }
+
     toggleFeedBox = () => {
-        this.setState({expanded: !this.state.expanded});
+        const willShow = !this.state.show;
+        const targetHeight = willShow ? 500 : 0;
+        this.setState({ show: willShow, mainStyles: { maxHeight: targetHeight }, animating: true });
+        this.mainDiv.addEventListener('transitionend', () => {
+            const scrollHeight = this.mainDiv.scrollHeight;
+            const finalHeight = Math.min(scrollHeight, targetHeight);
+            this.setState({ animating: false, mainStyles: { maxHeight: finalHeight } });
+        });
     }
 
     render() {
         return (
             <div className="feed-box">
-                <div className="feed-box-header" onClick={this.toggleFeedBox}>{this.props.title}</div>
-                <div className={`feed-box-main${this.state.expanded ? '' : ' hide'}`}>
-                    {this.props.children}
+                <div className="header" onClick={this.toggleFeedBox}>{this.props.title}</div>
+                <div
+                    ref={this.watchMain}
+                    className={`main ${this.state.show ? 'notslide-down' : 'notslide-up'}`}
+                    style={this.state.mainStyles}
+                >
+                    {this.state.show || this.state.animating ? this.props.children : null}
                 </div>
             </div>
         );
